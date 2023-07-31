9da81f2cd8774ef16b8991744ca775414c2e0d30
ahmid7 <ahmidsanni@gmail.com>
2023-07-31 22:06:48 +0100

    tablet responsiveness for header

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ const weeklyForecastData = [
 export default function Home() {
   return (
     <main className="bg-black">
-      <section className="px-10 space-y-9 pt-4">
+      <section className="px-8 xl:px-10 space-y-8 xl:space-y-9 pt-2 xl:pt-4">
         <div>
           <header className="flex items-center justify-between py-3.5">
             <div className="flex items-center gap-x-2 font-semibold text-xl xl:text-2xl">
@@ -50,8 +50,8 @@ export default function Home() {
                 alt="logo-image"
                 priority={true}
                 quality={100}
-                width={60}
-                height={60}
+                width={50}
+                height={50}
                 className="object-fit"
               />
 
@@ -71,11 +71,11 @@ export default function Home() {
               <input
                 type="text"
                 placeholder="Enter City"
-                className="bg-inherit flex-1 text-white outline-none border-none py-2 rounded-full text-opacity-80 text-sm"
+                className="bg-inherit flex-1 text-white outline-none border-none py-1 xl:py-2 rounded-full text-opacity-80 text-sm"
               />
             </div>
 
-            <button className="flex items-center space-x-3 rounded-full bg-[#575796] w-fit px-3 py-2.5 ">
+            <button className="flex items-center space-x-3 rounded-full bg-[#575796] w-fit px-3 py-2  xl:py-2.5 ">
               <Image
                 alt="location-icon"
                 priority={true}
@@ -85,7 +85,7 @@ export default function Home() {
                 height={20}
               />
 
-              <span>Current Location</span>
+              <span className="text-sm">Current Location</span>
             </button>
           </header>
         </div>
